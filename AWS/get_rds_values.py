1f17e6dd488cf20fb0cff11787b857c0b54782b0
Minkwon jung <43038052+Omtye@users.noreply.github.com>
2023-05-17 15:57:16 +0900

    Update get_rds_values.py

--- AWS/get_rds_values.py
+++ AWS/get_rds_values.py
@@ -3,10 +3,10 @@ import boto3
 def get_rds_values(value):
     rds_client = boto3.client("rds")
 
-    # 모든 RDS 클러스터 정보 가져오기
+    # get all cluster
     db_clusters = rds_client.describe_db_clusters()
 
-    # 클러스터 설정 값 가져오기
+    # get cluster values
     for cluster in db_clusters["DBClusters"]:
         cluster_id = cluster["DBClusterIdentifier"]
         print(cluster_id, ":", cluster[value])
